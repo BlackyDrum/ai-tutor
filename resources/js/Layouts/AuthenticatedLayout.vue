230069fbfc5daa6e45c1381ba76a98f2e3db1712
BlackyDrum <ganiaytan1010@web.de>
2024-02-09 19:32:43 +0100

    Add option to create new chat

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -4,15 +4,18 @@ import { router } from "@inertiajs/vue3";
 
 import ScrollPanel from "primevue/scrollpanel";
 import Avatar from "primevue/avatar";
+import ApplicationLogo from "@/Components/ApplicationLogo.vue";
 
 const showProfileOP = ref(false);
 const showResponsiveNavBar = ref(false);
+
+const appName = import.meta.env.VITE_APP_NAME;
 </script>
 
 <template>
     <div
         @click="showResponsiveNavBar = !showResponsiveNavBar"
-        class="fixed z-30 dark:text-white ml-2 mt-2 cursor-pointer"
+        class="fixed z-30 dark:text-white right-0 mr-3 mt-3 cursor-pointer"
         :class="{ 'text-white': showResponsiveNavBar }"
     >
         <span class="pi pi-bars"></span>
@@ -20,13 +23,29 @@ const showResponsiveNavBar = ref(false);
 
     <div class="flex">
         <div
-            class="h-screen w-[260px] flex-shrink-0 z-20 pt-10 bg-black text-white max-sm:fixed"
+            class="h-screen w-[260px] flex-shrink-0 z-20 bg-black text-white max-sm:fixed"
             :class="{ hidden: !showResponsiveNavBar }"
         >
             <nav class="h-full w-full p-2">
                 <div class="h-full w-full flex flex-col">
+                    <div
+                        @click="router.get('/')"
+                        class="flex hover:bg-app-light cursor-pointer p-2 rounded-lg"
+                    >
+                        <div>
+                            <ApplicationLogo class="w-8" />
+                        </div>
+                        <div class="self-center ml-3">
+                            {{ appName }}
+                        </div>
+                        <div class="ml-auto self-center">
+                            <span class="pi pi-pencil"></span>
+                        </div>
+                    </div>
                     <div class="flex-1 h-[60%]">
-                        <ScrollPanel class="w-full h-full"> </ScrollPanel>
+                        <ScrollPanel class="w-full h-full p-2">
+                            <p v-for="i in 100">Chat #{{ i }}</p>
+                        </ScrollPanel>
                     </div>
                     <div
                         v-if="showProfileOP"
