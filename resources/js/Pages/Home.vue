2a761feab7fea2f54bfc6edbd1f4a87c03260f00
BlackyDrum <ganiaytan1010@web.de>
2024-02-09 19:13:22 +0100

    Add input text area to write prompt

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -3,6 +3,7 @@ import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 
 import { Head } from "@inertiajs/vue3";
 import ApplicationLogo from "@/Components/ApplicationLogo.vue";
+import InputText from "primevue/inputtext";
 </script>
 
 <template>
@@ -12,10 +13,20 @@ import ApplicationLogo from "@/Components/ApplicationLogo.vue";
         <div
             class="w-full h-screen flex flex-col justify-center items-center dark:bg-app-light dark:text-white"
         >
-            <div class="mb-4 border rounded-full">
-                <ApplicationLogo class="w-16" />
+            <div
+                class="flex flex-col flex-1 items-center justify-center w-full"
+            >
+                <div class="mb-4 border rounded-full">
+                    <ApplicationLogo class="w-16" />
+                </div>
+                <div class="text-2xl font-bold">How can I help you?</div>
             </div>
-            <div class="text-2xl font-bold">How can I help you?</div>
+            <div class="mb-10 w-full text-center">
+                <InputText
+                    class="h-14 max-xl:w-3/4 w-1/2 bg-app-light rounded-lg dark:text-white"
+                    placeholder="Type your Message..."
+                />
+            </div>
         </div>
     </AuthenticatedLayout>
 </template>
