494dc225a9f6e9278f051ffc516cc398712e3e72
BlackyDrum <ganiaytan1010@web.de>
2024-02-14 18:14:12 +0100

    Add api authorization credentials

---
+++ config/api.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'username' => env('API_USERNAME'),
+
+    'password' => env('API_PASSWORD'),
+
+    'scope' => env('API_SCOPE'),
+
+    'grant_type' => env('API_GRANT_TYPE'),
+
+    'client_id' => env('API_CLIENT_ID'),
+
+    'client_secret' => env('API_CLIENT_SECRET'),
+];
