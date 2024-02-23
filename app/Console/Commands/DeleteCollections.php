a404868ace2bcf9f9ddc25d1f2ed278d16dd0da9
BlackyDrum <ganiaytan1010@web.de>
2024-02-23 23:48:07 +0100

    Add artisan command to delete all collections

---
+++ app/Console/Commands/DeleteCollections.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Files;
+use Codewithkyrian\ChromaDB\ChromaDB;
+use Illuminate\Console\Command;
+
+class DeleteCollections extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:delete-collections';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $chromaDB = ChromaDB::client();
+
+        $chromaDB->deleteAllCollections();
+
+        self::deleteDirectory(storage_path() . '/app/uploads');
+
+        Files::query()->delete();
+    }
+
+    private function deleteDirectory($dirPath) {
+        if (!is_dir($dirPath)) {
+            return;
+        }
+
+        if (!str_ends_with($dirPath, '/')) {
+            $dirPath .= '/';
+        }
+
+        $files = glob($dirPath . '*', GLOB_MARK);
+
+        foreach ($files as $file) {
+            if (is_dir($file)) {
+                self::deleteDirectory($file);
+            } else {
+                unlink($file);
+            }
+        }
+
+        rmdir($dirPath);
+    }
+}
