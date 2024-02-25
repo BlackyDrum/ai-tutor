14013c3f1af3570a42e820177827a4953e56b24c
BlackyDrum <ganiaytan1010@web.de>
2024-02-25 13:22:58 +0100

    Customize nova menu

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -2,8 +2,23 @@
 
 namespace App\Providers;
 
+use App\Models\Agents;
+use App\Models\Collections;
+use App\Models\Conversations;
+use App\Models\Messages;
+use App\Nova\Agent;
+use App\Nova\Collection;
+use App\Nova\Conversation;
+use App\Nova\Embedding;
+use App\Nova\Message;
+use App\Nova\User;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Gate;
+use Laravel\Nova\Badge;
+use Laravel\Nova\Dashboards\Main;
+use Laravel\Nova\Menu\MenuItem;
+use Laravel\Nova\Menu\MenuSection;
 use Laravel\Nova\Nova;
 use Laravel\Nova\NovaApplicationServiceProvider;
 
@@ -20,6 +35,31 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
 
         Nova::footer(function($request) {
             return Blade::render('FH Aachen - University of Applied Sciences');
+        });
+
+        Nova::mainMenu(function(Request $request) {
+            return [
+                MenuSection::dashboard(Main::class)->icon('chart-bar'),
+
+                MenuSection::make('Chat', [
+                    MenuItem::resource(Agent::class)
+                        ->withBadgeIf(fn() => 'No Active!', 'warning', fn() => Agents::query()->where('active', true)->count() === 0),
+                    MenuItem::resource(Conversation::class),
+                    MenuItem::resource(Message::class),
+                ])->icon('annotation')->collapsable(),
+
+                MenuSection::make('ChromaDB', [
+                    MenuItem::resource(Collection::class)
+                        ->withBadgeIf(fn() => 'No Collection!', 'warning', fn() => Collections::query()->count() === 0),
+                    MenuItem::resource(Embedding::class),
+                ])->icon('database')->collapsable(),
+
+                MenuSection::make('Users', [
+                    MenuItem::resource(User::class),
+                ])->icon('user')->collapsable(),
+
+                MenuItem::externalLink('Back to ' . config('app.name'), '/')
+            ];
         });
     }
 
