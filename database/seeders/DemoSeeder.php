8de9832e79fa913bcc8ffcb31ba7399a22b356d4
BlackyDrum <ganiaytan1010@web.de>
2024-03-08 18:06:56 +0100

    Add demo seeder

---
+++ database/seeders/DemoSeeder.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Http\Controllers\ChromaController;
+use App\Http\Controllers\HomeController;
+use App\Models\Agents;
+use App\Models\Collections;
+use App\Models\Modules;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+
+class DemoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Artisan::call('chroma:destroy');
+
+        $module = Modules::query()->create([
+            'name' => 'Demo',
+            'ref_id' => 1214757,
+            'temperature' => 0.7,
+            'max_tokens' => 1000
+        ]);
+
+        $user = User::query()->create([
+            'name' => 'admin',
+            'password' => '$2y$12$/NMljmWG.5fUFtpGtFihiu4N49eIoU.CYMRtH7YG6tCqaGTlXrsvm',
+            'admin' => true,
+            'max_requests' => 100,
+            'module_id' => $module->id,
+        ]);
+
+        $collection = Collections::query()->create([
+            'name' => 'DemoCollection',
+            'max_results' => 5,
+            'module_id' => $module->id,
+        ]);
+
+        $token = HomeController::getBearerToken();
+
+        if (is_array($token)) {
+            $collection->forceDelete();
+            $user->delete();
+            $module->delete();
+            abort(500, $token['reason']);
+        }
+
+        $response = Http::withToken($token)->withoutVerifying()->post(config('api.url') . '/agents/create-agent', [
+            'name' => 'DemoAgent_' . time(),
+            'context' => 'Testing GPT with Embeddings',
+            'first_message' => 'Hello.',
+            'response_shape' => 'Always provide code samples.',
+            'instructions' => 'You are a helpful university tutor providing aid for students tasked with programming relational database based web applications with php. always explain the code snippets you send and try to provide sources where to learn more on that subject. if in doubt, do not answer with code and ask to clarify the prompt!',
+            'creating_user' => config('api.username'),
+        ]);
+
+        if ($response->failed()) {
+            $collection->forceDelete();
+            $user->delete();
+            $module->delete();
+            abort(500, $response->reason());
+        }
+
+        $agent = Agents::query()->create([
+            'api_id' => $response->json()['id'],
+            'name' => 'DemoAgent_' . time(),
+            'context' => 'Testing GPT with Embeddings',
+            'first_message' => 'Hello.',
+            'response_shape' => 'Always provide code samples.',
+            'instructions' => 'You are a helpful university tutor providing aid for students tasked with programming relational database based web applications with php. always explain the code snippets you send and try to provide sources where to learn more on that subject. if in doubt, do not answer with code and ask to clarify the prompt!',
+            'active' => true,
+            'user_id' => $user->id,
+            'module_id' => $module->id,
+        ]);
+
+        $result = ChromaController::createCollection($collection->name);
+
+        if (!$result['status']) {
+            $collection->forceDelete();
+            $user->delete();
+            $module->delete();
+            $agent->delete();
+            abort(500, $result['message']);
+        }
+    }
+}
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" version="0.1.0">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="app-version" content="0.2.0">
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
