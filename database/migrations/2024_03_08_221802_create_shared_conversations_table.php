3b1c0583f10f1b616386cf204319f1f384028be3
BlackyDrum <ganiaytan1010@web.de>
2024-03-09 01:14:46 +0100

    Add unique constraint

--- database/migrations/2024_03_08_221802_create_shared_conversations_table.php
+++ database/migrations/2024_03_08_221802_create_shared_conversations_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('shared_conversations', function (Blueprint $table) {
             $table->id();
-            $table->string('url_identifier');
+            $table->string('url_identifier')->unique();
             $table->unsignedBigInteger('conversation_id')->unique();
             $table->timestamps();
 
