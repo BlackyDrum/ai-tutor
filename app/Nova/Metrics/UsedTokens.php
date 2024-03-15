a3b1155d30e03ae5be472cae9153b779c3001ef4
BlackyDrum <ganiaytan1010@web.de>
2024-03-15 14:15:26 +0100

    Add ranges for token metric

--- app/Nova/Metrics/UsedTokens.php
+++ app/Nova/Metrics/UsedTokens.php
@@ -3,6 +3,7 @@
 namespace App\Nova\Metrics;
 
 use App\Models\Messages;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Laravel\Nova\Http\Requests\NovaRequest;
@@ -20,13 +21,18 @@ class UsedTokens extends Value
     public function calculate(NovaRequest $request)
     {
         $result = Messages::query()
-            ->select(DB::raw('SUM(prompt_tokens + completion_tokens) as total'))
-            ->first();
+            ->select(DB::raw('SUM(prompt_tokens + completion_tokens) as total'));
+
+        if ($request->input('range') != 'ALL') {
+            $result->where('created_at', '>=', Carbon::now()->subDays($request->input('range')));
+        }
+
+        $result = $result->first();
 
         return $this->result($result->total);
     }
 
-    public $name = "Total Tokens";
+    public $name = 'Total Tokens';
 
     /**
      * Get the ranges available for the metric.
@@ -35,7 +41,15 @@ class UsedTokens extends Value
      */
     public function ranges()
     {
-        return [];
+        return [
+            7 => Nova::__('7 Days'),
+            14 => Nova::__('14 Days'),
+            30 => Nova::__('30 Days'),
+            60 => Nova::__('60 Days'),
+            90 => Nova::__('90 Days'),
+            180 => Nova::__('180 Days'),
+            'ALL' => Nova::__('All Time'),
+        ];
     }
 
     /**
