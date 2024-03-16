0890ef1b45d20a0a04092a8285e4ab52105bcedc
BlackyDrum <ganiaytan1010@web.de>
2024-03-17 00:19:06 +0100

    Update version

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="app-version" content="0.12.1">
+        <meta name="app-version" content="0.12.2">
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
