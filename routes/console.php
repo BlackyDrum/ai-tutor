ea99bf4785990d35fc46a471823170de46e1f14c
BlackyDrum <ganiaytan1010@web.de>
2024-03-29 23:09:01 +0100

    Remove auth tokens table

--- routes/console.php
+++ routes/console.php
@@ -1,11 +1,7 @@
 <?php
 
-use App\Models\AuthTokens;
-use Carbon\Carbon;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Schedule;
-
 /*
 |--------------------------------------------------------------------------
 | Console Routes
@@ -20,12 +16,3 @@ use Illuminate\Support\Facades\Schedule;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
-
-// Delete all expired auth tokens
-Schedule::call(function () {
-    $expireAfter = config('api.token_expiration');
-
-    AuthTokens::query()
-        ->where('created_at', '<', Carbon::now()->subSeconds($expireAfter))
-        ->delete();
-})->everyFiveMinutes();
