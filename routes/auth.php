b4333be16ea5bc152af25ef7eab43445dc1ed6c4
BlackyDrum <ganiaytan1010@web.de>
2024-03-29 15:04:40 +0100

    Remove old auth routes

--- routes/auth.php
+++ routes/auth.php
@@ -4,20 +4,6 @@ use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\LtiController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('auth')
-    ->name('auth.')
-    ->group(function () {
-        Route::get('/prepare-launch', [
-            AuthenticatedSessionController::class,
-            'prepareLaunch',
-        ])->name('prepare');
-
-        Route::get('/launch', [
-            AuthenticatedSessionController::class,
-            'launch',
-        ])->name('launch');
-    });
-
 Route::middleware('guest')->group(function () {
     Route::get('login', [
         AuthenticatedSessionController::class,
