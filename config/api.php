db890d466860d09c53d4201dc435452db582f63e
BlackyDrum <ganiaytan1010@web.de>
2024-04-09 21:10:35 +0200

    Remove old config variable

--- config/api.php
+++ config/api.php
@@ -10,6 +10,4 @@ return [
     'jina_api_key' => env('JINA_API_KEY'),
 
     'openai_api_key' => env('OPENAI_API_KEY'),
-
-    'token_expiration' => 20, // Expiration time of auth tokens after creation (in seconds)
 ];
