2233a2de681bf24d3f8e73bfeb977ce3611f92a1
BlackyDrum <ganiaytan1010@web.de>
2024-04-22 22:06:33 +0200

    Only show scrollbar on hover

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="app-version" content="1.2.0">
+        <meta name="app-version" content="1.2.1">
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
